cffa38e9420859a9843110e241be4513689aa431
Tomkas <76975378+Tomkas06@users.noreply.github.com>
2023-02-16 18:42:02 -0300

    Add files via upload

---
+++ Mi-CV/javaScript/script.js
@@ -0,0 +1,6 @@
+const botonMostrar=document.getElementById('botonMostrar');
+
+botonMostrar.onclick=function(){
+    document.getElementById('parrafo1').style.display="block";
+    document.getElementById('parrafo2').style.display="block";
+}
